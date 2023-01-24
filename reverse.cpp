9e555127195a32a0ce2b19085906338327d5b2ce
Lahari-00 <112242529+Lahari-00@users.noreply.github.com>
2023-01-24 13:41:18 +0530

    Add files via upload

---
+++ reverse.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num,rem,reverse;
+	cout<<"enter the number";
+	cin>>num;
+	while(num!=0)
+	{
+	    rem=num%10;
+	    reverse=reverse*10+rem;
+	    num=num/10;
+	}
+	cout<<"reverse number is"<<reverse;
+	return 0;
+	
+}
